d9c97152efa95d5ac1d6a3b08958fa44a70de57e
LAPTOP512\UdhayaKumar <udhya.kumar@amtexsystems.com>
2024-01-02 12:52:12 +0530

    Added css and js file in html

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(function() {
+    $("h1").addClass("header");
+    $("div").addClass("option");
+    $("#red").css("background-color" , "red");
+    $("#blue").css("background-color" , "blue");
+    $("#green").css("background-color" , "green");
+});
